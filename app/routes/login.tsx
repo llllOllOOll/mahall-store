514373c7b02f49ea577abb7f1589ac59ecf85c0e
seven <7b37b3@gmail.com>
2022-11-27 15:48:48 -0300

    resposive  login fix and layout

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -14,52 +14,53 @@ import userIcon from 'images/user_icon.svg'
 export default function Login() {
   return (
     <>
-      <header className="relative  max-w-sm  w-full h-14 bg-PrimaryBlue-500 text-white font-roboto text-2xl flex justify-center items-center">
-        <div >
-          <img className="absolute inset-0 mt-2 ml-4" src="images/ButtonArrow.svg" alt="" />
-        </div>
-        <div>Mahall Store</div>
-      </header>
-
-      <main className="max-w-sm w-full   bg-white flex   justify-start items-center flex-col">
-
-        <section>
-          <div className="mt-4  mb-4 flex justify-center items-center">
-            <img src="images/user_icon.svg" alt="" />
+      <div className="sticky top-0 z-30 max-w-md w-full min-h-screen ">
+        <header className="max-w-md  w-full h-14  text-PrimaryBlue-500 bg-gray-100 font-roboto font-bold text-2xl flex justify-center items-center">
+          <div>
+            <img className="absolute inset-0 mt-2 ml-4 hidden" src="images/ButtonArrow.svg" alt="" />
           </div>
-        </section>
+          <div>Mahall Store</div>
+        </header>
 
-        <section className="w-full max-w-sm px-4">
-          <Form className="bg-white font-roboto font-bold  text-base text-gray-400  px-4  py-4 rounded-md shadow-lg " method="post">
-            <div>
-              <label className="block" htmlFor="email">
-                Login
-              </label>
-              <input className="w-full border h-12 mt-1 mb-2 rounded-lg" type="email" name="email" id="email" required />
-            </div>
-            <div>
-              <label className="block" htmlFor="password">
-                Senha
-              </label>
-              <input className="w-full border h-12 mt-1 rounded-lg" id="password" type="password" name="password" autoComplete="current-password"
-                required
-              />
-            </div>
-            <div>
-              <button className="w-full h-12 rounded-lg bg-PrimaryBlue-500 text-white mt-8" >Acessar</button>
-            </div>
-            <div>
-              <button className="w-full h-12 rounded-lg bg-PrimaryBlue-400 text-white mt-8 mb-8"  >Criar novo login</button>
-            </div>
-            <p className="text-center" >Esqueci minha senha</p>
-          </Form>
-        </section>
-      </main>
+        <main className=" max-w-md w-full bg-white flex   justify-start items-center flex-col">
 
-      <footer className="w-full max-w-sm">
+          <section>
+            <div className="mt-4  mb-4 flex justify-center items-center">
+              <img src="images/user_icon.svg" alt="" />
+            </div>
+          </section>
+
+          <section className="w-full max-w-md px-4">
+            <Form className="bg-white font-roboto font-bold  text-base text-gray-400  px-4  py-4 rounded-md  " method="post">
+              <div>
+                <label className="block" htmlFor="email">
+                  Login
+                </label>
+                <input className="w-full border h-12 mt-1 mb-2 rounded-lg px-3 " type="email" name="email" id="email" required />
+              </div>
+              <div>
+                <label className="block" htmlFor="password">
+                  Senha
+                </label>
+                <input className="w-full border h-12 mt-1 rounded-lg px-3" id="password" type="password" name="password" autoComplete="current-password"
+                  required
+                />
+              </div>
+              <div>
+                <button className="w-full h-12 rounded-lg bg-PrimaryBlue-500 text-white mt-8" >Acessar</button>
+              </div>
+              <div>
+                <button className="w-full h-12 rounded-lg bg-PrimaryBlue-400 text-white mt-8 mb-8"  >Criar novo login</button>
+              </div>
+              <p className="text-center" >Esqueci minha senha</p>
+            </Form>
+          </section>
+        </main>
+      </div>
+      {/* <footer className="w-full max-w-md">
         <div className="w-full h-12 bg-PrimaryBlue-500 font-roboto text-2xl">
         </div>
-      </footer>
+      </footer> */}
     </>
   );
 }
